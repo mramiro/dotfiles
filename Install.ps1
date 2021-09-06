891dbc458edd0f729712daf8b66a43e251d3d500
Miguel Ramiro <miguel@mramiro.com>
2021-09-06 04:46:21 -0700

    Windows has multiple folders for settings

--- Install.ps1
+++ Install.ps1
@@ -4,17 +4,22 @@
   [Parameter(Mandatory=$false)][Switch]$Force
 )
 
-$srcFolder = Get-Item "$PSScriptRoot/windows"
-$localAppData = [Environment]::GetFolderPath("LocalApplicationData")
-Get-ChildItem -Recurse -File $srcFolder | ForEach {
-  $srcFile = $_
-  $relPath = [System.IO.Path]::GetRelativePath($srcFolder, $srcFile)
-  $targetPath = Join-Path $localAppData $relPath
-  if (!$Force -and (Test-Path -Type Leaf -Path $targetPath)) {
-    Write-Host "File exists. Skipping: $targetPath"
-  }
-  Write-Host "Copying file: $srcFile -> $targetPath"
-  if ($PSCmdlet.ShouldProcess($targetPath, "Copy file $srcFile")) {
-    Copy-Item $srcFile $targetPath
+function InstallToWinFolder([System.IO.DirectoryInfo]$srcFolder) {
+  # "MyDocuments", "LocalApplicationData", etc.
+  $baseTargetFolder = [Environment]::GetFolderPath($srcFolder.BaseName)
+  Get-ChildItem -Recurse -File $srcFolder | ForEach {
+    $srcFile = $_
+    $relPath = [System.IO.Path]::GetRelativePath($srcFolder, $srcFile)
+    $targetPath = Join-Path $baseTargetFolder $relPath
+    if (!$Force -and (Test-Path -Type Leaf -Path $targetPath)) {
+      Write-Host "File exists. Skipping: $targetPath"
+    }
+    Write-Host "Copying file: $srcFile -> $targetPath"
+    if ($PSCmdlet.ShouldProcess($targetPath, "Copy file $srcFile")) {
+      Copy-Item $srcFile $targetPath
+    }
   }
 }
+
+$rootFolder = Get-Item "$PSScriptRoot/windows"
+Get-ChildItem -Directory $rootFolder | ForEach-Object { InstallToWinFolder $_ }
